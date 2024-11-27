8e030d9d79a2a4fa8a2fafa775ea0c5280e1bf08
Henrik Ekblad <henrik.ekblad@gmail.com>
2024-11-27 11:01:36 +0100

    add build script

---
+++ build.js
@@ -0,0 +1,18 @@
+
+import shell from 'shelljs';
+
+if (!shell.which('wasm-pack')) {
+	console.error("Run npm install to install all dependencies first");
+}
+
+// Clean up any existing build
+shell.rm('-rf', 'rust/pkg');
+
+// Create the node output
+shell.exec('wasm-pack build ./rust --target nodejs --out-dir pkg/nodejs --release');
+
+// Create the web output
+shell.exec('wasm-pack build ./rust --target web --out-dir pkg/web --release');
+
+// Clean up
+shell.rm('rust/pkg/*/{package.json,.gitignore}');
