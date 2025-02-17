5f8f8123bd9d716acea96d2b4c127c144d9b5e39
Kyota Nagao <kyota.nagao@estie.co.jp>
2025-02-17 10:07:59 +0900

    chore: add print options tests

---
+++ test/print.test.ts
@@ -0,0 +1,54 @@
+import { Color, Format, Workbook } from "../web";
+import { describe, test, beforeAll, expect } from "vitest";
+import { initWasModule, readXlsx, readXlsxFile } from "./common";
+
+beforeAll(async () => {
+  await initWasModule();
+});
+
+describe("xlsx-wasm test", () => {
+  test("worksheet print", async () => {
+    // Arrange
+    const workbook = new Workbook();
+
+    // Act
+    const worksheet = workbook.addWorksheet();
+
+    const purple = Color.purple();
+    const format = new Format().setFontColor(purple);
+    worksheet.writeWithFormat(0, 0, "Hello, World!", format);
+
+    worksheet.setLandscape();
+    worksheet.setScreenGridlines(false);
+    worksheet.setPrintGridlines(true);
+    worksheet.setPrintArea(0, 0, 10, 10);
+    worksheet.setPrintScale(110);
+    worksheet.setPaperSize(9); // A4
+    worksheet.setPrintBlackAndWhite(true);
+    worksheet.setPrintFirstPageNumber(0);
+    worksheet.setPrintDraft(true);
+    worksheet.setPrintCenterHorizontally(true);
+    worksheet.setPrintCenterVertically(true);
+    worksheet.setPrintHeadings(true);
+
+    // Assert
+    const actual = await readXlsx(workbook.saveToBufferSync());
+    const expected = await readXlsxFile("./expected/print.xlsx");
+    expect(actual).matchXlsx(expected);
+  });
+
+  test("worksheet print portrait", async () => {
+    // Arrange
+    const workbook = new Workbook();
+
+    // Act
+    const worksheet = workbook.addWorksheet();
+    worksheet.write(0, 0, "Hello, World!");
+    worksheet.setPortrait();
+
+    // Assert
+    const actual = await readXlsx(workbook.saveToBufferSync());
+    const expected = await readXlsxFile("./expected/print_portrait.xlsx");
+    expect(actual).matchXlsx(expected);
+  });
+});
