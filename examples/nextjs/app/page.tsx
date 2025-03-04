ac7b6be0cffd13b86f4fd3beb6de010f0b8eec4b
Yohta Kimura <38206553+rajyan@users.noreply.github.com>
2025-03-04 22:54:30 +0900

    fix: disable ssr

--- examples/nextjs/app/page.tsx
+++ examples/nextjs/app/page.tsx
@@ -1,34 +1,4 @@
 "use client";
-import initModule, { Workbook } from "wasm-xlsxwriter";
-
-export default function Page() {
-  return (
-    <button
-      onClick={async () => {
-        const workbook = await generateXlsx();
-        download(workbook);
-      }}
-    >
-      Generate a xlsx file
-    </button>
-  );
-}
-
-const generateXlsx = async () => {
-  await initModule();
-  const workbook = new Workbook();
-  const worksheet = workbook.addWorksheet();
-  worksheet.write(0, 0, "Hello, world!");
-  return workbook;
-};
-
-const download = (workbook: Workbook) => {
-  const buffer = workbook.saveToBufferSync();
-  const blob = new Blob([buffer]);
-  const url = window.URL.createObjectURL(blob);
-  const a = document.createElement("a");
-  a.href = url;
-  a.download = "generated.xlsx";
-  a.click();
-  a.remove();
-};
+import dynamic from "next/dynamic";
+const Page = dynamic(() => import("./PageComponent"), { ssr: false });
+export default Page;
