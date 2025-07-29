0a3f23b137784c4464bae4cd16822fe6189e1563
Yohta Kimura <38206553+rajyan@users.noreply.github.com>
2025-07-29 10:06:16 +0900

    tmp

--- rust/tools/wrapper_generator/src/main.rs
+++ rust/tools/wrapper_generator/src/main.rs
@@ -44,6 +44,8 @@ fn main() -> Result<()> {
     let common_methods = generate_common_methods(&struct_info);
     let wrapper_methods = generate_wrapper_methods(&struct_info);
 
+    let from_impls = generate_impl_from(struct_info.name());
+
     let mut impl_block = Item::from(Impl::simple(
         Type::Path(Path::single(PathSegment::new(struct_info.name(), None))),
         common_methods.into_iter().chain(wrapper_methods).collect(),
@@ -59,6 +61,9 @@ fn main() -> Result<()> {
         krate.add_item(use_item);
     });
     krate.add_item(struct_);
+    from_impls.into_iter().for_each(|from_impl| {
+        krate.add_item(from_impl);
+    });
     krate.add_item(impl_block);
 
     println!("{}", krate);
@@ -127,6 +132,73 @@ fn generate_struct_wrapper(struct_info: &StructItem) -> Item<ItemKind> {
     )));
 
     item
+}
+
+fn generate_impl_from(struct_name: &str) -> Vec<Item<ItemKind>> {
+    // First implementation: impl From<StructName> for xlsx::StructName
+    let from_impl1 = Impl::new(
+        vec![],
+        Some(Type::Path(Path::single(PathSegment::new("From", Some(vec![GenericArg::Type(
+            Type::Path(Path::single(PathSegment::new(struct_name, None))),
+        )]))))),
+        Type::Path(Path::single(PathSegment::new("xlsx", None)).chain(PathSegment::new(struct_name, None))),
+        None,
+        vec![AssocItem::from(AssocItemKind::Fn(Fn::simple(
+            "from",
+            FnDecl::regular(
+                vec![Param::new(Pat::ident("format"), Type::Path(Path::single(struct_name)))],
+                Some(Type::Path(Path::single("Self")))
+            ),
+            Block::single(
+                ExprKind::method_call0(
+                    ExprKind::from(ExprKind::method_call0(
+                        ExprKind::Path(Path::single("format")),
+                        "lock"
+                    )),
+                    "clone"
+                )
+            ),
+        )))],
+    );
+
+    // Second implementation: impl From<StructName> for &'static xlsx::StructName
+    let from_impl2 = Impl::new(
+        vec![],
+        Some(Type::Path(Path::single(PathSegment::new("From", Some(vec![GenericArg::Type(
+            Type::Path(Path::single(PathSegment::new(struct_name, None))),
+        )]))))),
+        Type::static_ref(Type::Path(Path::single(PathSegment::new("xlsx", None)).chain(PathSegment::new(struct_name, None)))),
+        None,
+        vec![AssocItem::from(AssocItemKind::Fn(Fn::simple(
+            "from",
+            FnDecl::regular(
+                vec![Param::new(Pat::ident("format"), Type::Path(Path::single(struct_name)))],
+                Some(Type::Path(Path::single("Self")))
+            ),
+            Block::single(
+                ExprKind::call(
+                    ExprKind::Path(Path::single("Box").chain("leak")),
+                    vec![
+                        Expr::from(ExprKind::call(
+                            ExprKind::Path(Path::single("Box").chain("new")),
+                            vec![
+                                Expr::from(ExprKind::method_call0(
+                                    ExprKind::from(ExprKind::method_call0(
+                                        ExprKind::Path(Path::single("format")),
+                                        "lock"
+                                    )),
+                                    "clone"
+                                ))
+                            ]
+                        ))
+                    ]
+                )
+            ),
+        )))],
+    );
+
+    // Return both implementations as a vector
+    vec![Item::from(from_impl1), Item::from(from_impl2)]
 }
 
 fn get_crate_info(manifest_path: &str, document_private: bool) -> Result<crate_inspector::Crate> {
